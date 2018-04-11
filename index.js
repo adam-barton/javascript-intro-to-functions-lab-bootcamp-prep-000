d090f18129d77aaf8934f8822c903983e1c38afc
Adam Barton <adam.barton333@gmail.com>
2018-04-11 02:25:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ function logWhisper(string) {
   console.log('hello')
 }
 function sayHiToGrandma(string) {
-  console.log("hello")
-  
-  return ("I can't hear you")
+  console.log('hello')
+   return ("I can't hear you!")
   
 }
