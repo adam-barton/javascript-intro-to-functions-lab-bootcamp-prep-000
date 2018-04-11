33aa8e52e37ce29494f4e7d60b696abfae3a3d97
Adam Barton <adam.barton333@gmail.com>
2018-04-11 02:18:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log('hello')
 }
 function sayHiToGrandma(string) {
-  console.log('hello')
+  console.log('hello''HELLO')
   
-  return ("I can't hear you!")
+  return ("I can't hear you!" "YES INDEED!")
 }
